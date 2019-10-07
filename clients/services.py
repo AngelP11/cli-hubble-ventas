efb742cc9fee07d8a544a0b3765d3b3e05c03eca
AngelP11 <angelgabrielponce@gmail.com>
2019-10-07 13:58:55 -0400

    Feature: Method Update a Delete is running correctly

--- clients/services.py
+++ clients/services.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from clients.models import Client
 
 
@@ -17,3 +18,36 @@ class ClientService:
             reader = csv.DictReader(f, fieldnames=Client.schema())
 
             return list(reader)
+
+    def update_client(self, updated_client):
+        clients_list = self.list_clients()
+
+        updated_clients = []
+
+        for client in clients_list:
+            if client['uid'] == updated_client.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+
+        self._save_to_disk(updated_clients)
+
+    def delete_client(self, deleted_client):
+        clients_list = self.list_clients()
+
+        for idx, client in enumerate(clients_list):
+            if client['uid'] == deleted_client.uid:
+                del clients_list[idx]
+                break
+
+        self._save_to_disk(clients_list)
+
+    def _save_to_disk(self, clients):
+        tmp_table_name = self.table_name + '.tmp'
+
+        with open(tmp_table_name, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(clients)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
